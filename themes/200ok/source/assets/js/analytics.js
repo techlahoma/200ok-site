b7912a348c63a6782d5f3d1675b67f020447387b
Maxx Crawford <mcrawford@broco.com>
2017-06-05 12:01:41 -0500

    Updated GA code to 200ok proprty ID

--- themes/200ok/source/assets/js/analytics.js
+++ themes/200ok/source/assets/js/analytics.js
@@ -5,5 +5,5 @@
   m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m);
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-100326405-1', 'auto');
+ga('create', 'UA-20201601-6', 'auto');
 ga('send', 'pageview');
